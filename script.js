3a87ab51b8c653c146a8b9af295edadcf7173bfe
jurypeak <101064769+jurypeak@users.noreply.github.com>
2023-09-20 23:50:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+  function change1(){
+document.getElementById("img1").src = "media/popSquare2.jpg";
+}
+  function change2(){
+document.getElementById("img2").src = "media/rockSquare2.jpg";
+}
+  function revertback1(){
+ document.getElementById("img1").src = "media/popSquare1.jpg";
+}
+  function revertback2(){
+ document.getElementById("img2").src = "media/rockSquare1.jpg";
+}
+	function change3(){
+document.getElementById("img3").src = "media/metalSquare2.jpg";
+}
+  function revertback3(){
+document.getElementById("img3").src = "media/metalSquare1.jpg";
+}
+  function change4(){
+ document.getElementById("img4").src = "media/workoutSquare2.jpg";
+}
+  function revertback4(){
+ document.getElementById("img4").src = "media/workoutSquare1.jpg";
+}
+	function change5(){
+document.getElementById("img5").src = "media/hiphopSquare2.jpg";
+}
+  function revertback5(){
+document.getElementById("img5").src = "media/hiphopSquare1.jpg";
+}
+  function change6(){
+ document.getElementById("img6").src = "media/countrySquare2.jpg";
+}
+  function revertback6(){
+ document.getElementById("img6").src = "media/countrySquare1.jpg";
+}
+	function change7(){
+document.getElementById("img7").src = "media/danceSquare2.jpg";
+}
+  function revertback7(){
+document.getElementById("img7").src = "media/danceSquare1.jpg";
+}
+  function change8(){
+ document.getElementById("img8").src = "media/classicalSquare2.jpg";
+}
+  function revertback8(){
+ document.getElementById("img8").src = "media/classicalSquare1.jpg";
+}
+	function change9(){
+document.getElementById("img9").src = "media/bluesSquare2.jpg";
+}
+  function revertback9(){
+document.getElementById("img9").src = "media/bluesSquare1.jpg";
+}
+  function change10(){
+ document.getElementById("img10").src = "media/dubstepSquare2.jpg";
+}
+  function revertback10(){
+ document.getElementById("img10").src = "media/dubstepSquare1.jpg";
+}
